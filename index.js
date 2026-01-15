a04521d37879687a3093b112dddffaf48e48ec2c
FreeFallRush <queenofcups.altbot@gmail.com>
2026-01-15 12:48:19 +0200

    Rewrite project using express

--- index.js
+++ index.js
@@ -1,43 +1,74 @@
-const http = require("http");
+// const http = require("http");
+// const path = require("path");
+// const fs = require("fs");
+
+// const PORT = 8080;
+
+// const server = http.createServer((req, res) => {
+//   let filePath;
+
+//   switch (req.url) {
+//     case "/":
+//       filePath = "index.html";
+//       res.statusCode = 200;
+//       break;
+//     case "/about":
+//       filePath = "about.html";
+//       res.statusCode = 200;
+//       break;
+//     case "/contact-me":
+//       filePath = "contact-me.html";
+//       res.statusCode = 200;
+//       break;
+//     default:
+//       filePath = "404.html";
+//       res.statusCode = 404;
+//       break;
+//   }
+
+//   fs.readFile(path.join(__dirname, filePath), "utf8", (err, data) => {
+//     if (err) {
+//       res.writeHead(500, { "Content-Type": "text/plain" });
+//       res.end("Server error");
+//       return;
+//     }
+
+//     res.writeHead(res.statusCode, { "Content-Type": "text/html" });
+//     res.end(data);
+//   });
+// });
+
+// server.listen(PORT, () => {
+//   console.log(`Server running at http://localhost:${PORT}`);
+// });
+
+//////////////
+//Express
+
+const express = require("express");
 const path = require("path");
-const fs = require("fs");
+
+const app = express();
 
 const PORT = 8080;
 
-const server = http.createServer((req, res) => {
-  let filePath;
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
-  switch (req.url) {
-    case "/":
-      filePath = "index.html";
-      res.statusCode = 200;
-      break;
-    case "/about":
-      filePath = "about.html";
-      res.statusCode = 200;
-      break;
-    case "/contact-me":
-      filePath = "contact-me.html";
-      res.statusCode = 200;
-      break;
-    default:
-      filePath = "404.html";
-      res.statusCode = 404;
-      break;
+app.get("/about", (req, res) =>
+  res.sendFile(path.join(__dirname, "about.html"))
+);
+
+app.get("/contact-me", (req, res) =>
+  res.sendFile(path.join(__dirname, "contact-me.html"))
+);
+
+app.use((req, res) =>
+  res.status(404).sendFile(path.join(__dirname, "404.html"))
+);
+
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
   }
-
-  fs.readFile(path.join(__dirname, filePath), "utf8", (err, data) => {
-    if (err) {
-      res.writeHead(500, { "Content-Type": "text/plain" });
-      res.end("Server error");
-      return;
-    }
-
-    res.writeHead(res.statusCode, { "Content-Type": "text/html" });
-    res.end(data);
-  });
-});
-
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`My first Express app - listening on port ${PORT}!`);
 });
